0ca4041aeaff920b63101a3bd6d5bae366ba99e2
dimitreandrew@gmail.com <dimitreandrew@gmail.com>
2018-09-30 18:27:06 -0300

    updated dataset script

--- dataset_generation.py
+++ dataset_generation.py
@@ -2,19 +2,23 @@ from dataset import parse_data, add_time_features
 
 
 # Script to load, parse and process data.
-
-unwanted_columns = ['adwordsClickInfo', 'fullVisitorId', 'sessionId', 'visitId', 'visitStartTime', 'browser',
-                    'browserSize', 'browserVersion', 'flashVersion', 'mobileDeviceInfo', 'mobileDeviceMarketingName',
-                    'mobileDeviceModel', 'mobileInputSelector', 'operatingSystemVersion', 'screenColors', 'metro',
-                    'networkDomain', 'networkLocation', 'adContent', 'campaign', 'isTrueDirect', 'keyword',
-                    'referralPath', 'source', 'operatingSystem', 'medium', 'channelGrouping', 'date']
+unwanted_columns = ['channelGrouping', 'fullVisitorId', 'sessionId', 'socialEngagementType', 'visitId',
+                    'visitStartTime', 'browser', 'browserSize', 'browserVersion', 'deviceCategory', 'flashVersion',
+                    'language', 'mobileDeviceBranding', 'mobileDeviceInfo', 'mobileDeviceMarketingName',
+                    'mobileDeviceModel', 'mobileInputSelector', 'operatingSystem', 'operatingSystemVersion',
+                    'screenColors', 'screenResolution', 'city', 'cityId', 'metro', 'networkDomain', 'networkLocation',
+                    'adContent', 'adwordsClickInfo', 'campaign', 'isTrueDirect', 'keyword', 'medium',
+                    'referralPath', 'source', 'latitude', 'longitude']
 
 train = parse_data('data/train_raw.csv')
+train.drop(['campaignCode'], axis=1, inplace=True)
+train.drop(unwanted_columns, axis=1, inplace=True)
 train = add_time_features(train)
-train = train.drop(unwanted_columns, axis=1)
+train.drop(['date'], axis=1, inplace=True)
 train.to_csv('data/train.csv', index=False)
 
 test = parse_data('data/test_raw.csv')
+test.drop(unwanted_columns, axis=1, inplace=True)
 test = add_time_features(test)
-test = test.drop(unwanted_columns, axis=1)
+test.drop(['date'], axis=1, inplace=True)
 test.to_csv('data/test.csv', index=False)

fixed script for basic numerical columns

dataset_generation.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 from dataset import parse_data, add_time_features
 
 
@@ -28,19 +29,24 @@ def split_data(input_data_path='data/train.csv', train_data_path='data/tf_train.
 
 
 # Script to load, parse and process data.
-unwanted_columns = ['channelGrouping', 'fullVisitorId', 'sessionId', 'socialEngagementType', 'visitId',
+unwanted_columns = ['channelGrouping', 'sessionId', 'socialEngagementType', 'visitId',
                     'visitStartTime', 'browser', 'browserSize', 'browserVersion', 'deviceCategory', 'flashVersion',
                     'language', 'mobileDeviceBranding', 'mobileDeviceInfo', 'mobileDeviceMarketingName',
                     'mobileDeviceModel', 'mobileInputSelector', 'operatingSystem', 'operatingSystemVersion',
                     'screenColors', 'screenResolution', 'city', 'cityId', 'metro', 'networkDomain', 'networkLocation',
                     'adContent', 'adwordsClickInfo', 'campaign', 'isTrueDirect', 'keyword', 'medium',
-                    'referralPath', 'source', 'latitude', 'longitude', 'continent', 'country', 'region', 'subContinent']
+                    'referralPath', 'source', 'latitude', 'longitude', 'continent', 'country', 'region', 'subContinent',
+                    'isMobile']
 
 train = parse_data('data/train_raw.csv')
 train.drop(['campaignCode'], axis=1, inplace=True)
 train.drop(unwanted_columns, axis=1, inplace=True)
 train = add_time_features(train)
 train.drop(['date'], axis=1, inplace=True)
+train["transactionRevenue"] = train["transactionRevenue"].astype('float')
+train["transactionRevenue"].fillna(0, inplace=True)
+train["transactionRevenue"] = np.log1p(train["transactionRevenue"])
+train.dropna(inplace=True)
 train.to_csv('data/train.csv', index=False)
 
 test = parse_data('data/test_raw.csv')